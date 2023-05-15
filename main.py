7c220c7b1733fa6a32c261cf0ee38b223c225472
FlipSide8404 <21422006-FlipSide8404@users.noreply.replit.com>
2023-05-15 21:53:52 +0000

    added comments

--- main.py
+++ main.py
@@ -15,3 +15,4 @@ with open(file_name, "r") as f:
     contents = f.read()
     print("File contents:")
     print(contents)
+#Note that this program assumes that the file name entered by the user is valid and can be written to and read from the current working directory. It also takes that the user enters their name, street address, and phone number in valid formats.
